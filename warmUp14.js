3e37ec4bb2ce7a52b73338f57403090b6c7499ec
insafmechergui <insaf_mechergui@outlook.com>
2019-11-02 09:53:54 +0100

    wup14

--- warmUp14.js
+++ warmUp14.js
@@ -1,7 +1,8 @@
 /*
 Say you have an array for which the i element is the price of a given stock on day i.
 
-If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), 
+design an algorithm to find the maximum profit.
 
 Note that you cannot sell a stock before you buy one.
 
@@ -19,5 +20,28 @@ Explanation: In this case, no transaction is done, i.e. max profit = 0.
 */
 
 var maxProfit = function(prices) {
-  // YOUR CODE HERE
+  // first part is about fcomparing the max profit per day in all days
+  var profit =[]
+
+  for (var i = 0; i < prices.length; i++) {
+  	for (var j =i; j < prices.length; j++) {	
+  		profit.push(prices[j]-prices[i])
+
+  		//console.log(prices[i],prices[j])
+  	}profit.push([])
+  	//console.log(prices[j],prices[j])
+  }
+
+// now we need to find the max number in the profit 
+  profit =profit.flat()
+  var maxProf = 0;
+  for (var i = 0; i < profit.length; i++) {
+  	for (var i = j; j < profit.length; j++) {
+  		if(profit[0] < profit[j] ){
+  			profit[0] = profit[j];
+  		}
+  	}  	
+  }
+return profit[0];
+
 };
